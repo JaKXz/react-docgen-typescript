74b4f32e6166d3fc6fe7d367dfee42d208766704
John Collett <john.collett@shopify.com>
2022-07-08 10:07:21 -0400

    remove only from test

--- src/__tests__/parser/plainClasses.test.ts
+++ src/__tests__/parser/plainClasses.test.ts
@@ -1,10 +1,9 @@
-import { string } from "prop-types";
 import { describe, expect, it } from "vitest";
 import { parse } from "../../parser";
 import { fixturePath } from "../testUtils";
 
-describe.only("parser > integration tests for plain classes", () => {
-  it.only("parses a plain class", () => {
+describe("parser > integration tests for plain classes", () => {
+  it("parses a plain class", () => {
     const result = parse(fixturePath("plain/class"));
 
     expect(result).toEqual([
